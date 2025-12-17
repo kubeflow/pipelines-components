eccb36b725460ace1dae81406e89ca2a15f6971d
Giulio Frasca <gfrasca@redhat.com>
2025-12-17 11:57:22 -0500

    chore: Linting fixes for validate metadata script
    Signed-off-by: Giulio Frasca <gfrasca@redhat.com>

--- scripts/validate_metadata/validate_metadata.py
+++ scripts/validate_metadata/validate_metadata.py
@@ -1,11 +1,11 @@
 import argparse
-import sys
-from itertools import pairwise
-import yaml
-from datetime import datetime, timezone
-from pathlib import Path
 import logging
+import sys
+from datetime import datetime, timezone
+from itertools import pairwise
+from pathlib import Path
 
+import yaml
 from semver import Version
 
 # The following ordered fields are required in a metadata.yaml file.
@@ -19,23 +19,32 @@ STABILITY_OPTIONS = ["alpha", "beta", "stable"]
 # 'Dependencies' must contain 'kubeflow' and can contain 'external_services'.
 DEPENDENCIES_FIELDS = ["kubeflow", "external_services"]
 # A given dependency must contain 'name' and 'version' fields.
-DEPENDENCY_REQUIRED_FIELDS=["name", "version"]
+DEPENDENCY_REQUIRED_FIELDS = ["name", "version"]
 # Comparison operators for dependency versions.
 COMPARISON = {">=", "<=", "=="}
 
-OWNERS="OWNERS"
-METADATA="metadata.yaml"
+OWNERS = "OWNERS"
+METADATA = "metadata.yaml"
+
 
 class ValidationError(Exception):
-    """Custom exception for validation errors that should be displayed
-    without traceback and can take a custom message.
+    """Custom exception for validation errors that should be displayed without traceback.
+
+    This exception can take a custom message.
     """
+
     def __init__(self, message: str = "A validation error occurred."):
+        """Initialize the ValidationError with a custom message.
+
+        Args:
+            message: The error message to display.
+        """
         # Call the base class constructor with the message
         super().__init__(message)
 
         # Store the message in an attribute (optional, but good practice)
         self.message = message
+
 
 def parse_args() -> argparse.Namespace:
     """Parse and validate command-line arguments.
@@ -49,17 +58,18 @@ def parse_args() -> argparse.Namespace:
         epilog="""
   # For example, from project root:
   python -m scripts.validate_metadata --dir components/data_processing/sample_component
-        """
+        """,
     )
 
     parser.add_argument(
-        '--dir',
+        "--dir",
         type=validate_dir,
         required=True,
-        help='Path to the component or pipeline directory (must contain OWNERS and metadata.yaml files)'
+        help="Path to the component or pipeline directory (must contain OWNERS and metadata.yaml files)",
     )
 
     return parser.parse_args()
+
 
 def validate_dir(path: str) -> Path:
     """Validate that the input path is a valid directory and contains required files.
@@ -90,6 +100,7 @@ def validate_dir(path: str) -> Path:
 
     return path
 
+
 def validate_owners_file(filepath: Path):
     """Validate that the OWNERS file contains at least one approver under the 'approvers' heading.
 
@@ -111,6 +122,7 @@ def validate_owners_file(filepath: Path):
     # If this line is reached, no approvers were found.
     raise ValidationError(f"OWNERS file at {filepath} requires 1+ approver under heading 'approvers:'.")
 
+
 def validate_metadata_yaml(filepath: Path):
     """Validate that the input filepath represents a metadata.yaml file with a valid schema.
 
@@ -127,14 +139,20 @@ def validate_metadata_yaml(filepath: Path):
 
         # Validate metadata.yaml has been verified within one year of the current date.
         if "lastVerified" not in metadata:
-            raise ValidationError(f"Metadata at {filepath} has corresponding metadata.yaml with no 'lastVerified' value.")
+            raise ValidationError(
+                f"Metadata at {filepath} has corresponding metadata.yaml with no 'lastVerified' value."
+            )
 
         last_verified = metadata.get("lastVerified")
         if not validate_date_verified(last_verified):
-            raise ValidationError(f"Metadata at {filepath} has corresponding metadata.yaml with invalid 'lastVerified' value: {last_verified}.")
+            raise ValidationError(
+                f"Metadata at {filepath} has corresponding metadata.yaml with invalid "
+                f"'lastVerified' value: {last_verified}."
+            )
 
         # Validate required fields and their corresponding values.
         validate_required_fields(metadata)
+
 
 def validate_date_verified(last_verified: datetime) -> bool:
     """Validate that the input date is RFC-3339-formatted and within 1 year of the current date.
@@ -162,9 +180,11 @@ def validate_date_verified(last_verified: datetime) -> bool:
         return False
     return True
 
+
 def validate_required_fields(metadata: dict):
-    """Validates that all required fields are present in the input dictionary and have valid values,
-    and that no invalid fields are present.
+    """Validates that all required fields are present in the input dictionary and have valid values.
+
+    Also validates that no invalid fields are present.
 
     Args:
         metadata: dictionary object containing nested metadata fields.
@@ -185,7 +205,9 @@ def validate_required_fields(metadata: dict):
         raise ValidationError(f"Missing required field 'name' in {METADATA}.")
     if not isinstance(name, str):
         type_name = type(name).__name__
-        raise ValidationError(f"{type_name} value identified in field 'name' in {METADATA}: '{name}'. Value for 'name' must be string.")
+        raise ValidationError(
+            f"{type_name} value identified in field 'name' in {METADATA}: '{name}'. Value for 'name' must be string."
+        )
 
     # Convert metadata keys to a set and compare against REQUIRED_FIELDS set.
     input_fields_set = set(input_metadata_fields)
@@ -199,7 +221,9 @@ def validate_required_fields(metadata: dict):
             raise ValidationError(f"Unexpected field(s) in {METADATA} for '{name}': {extra_fields}.")
     # Compare input fields against REQUIRED FIELDS as lists to verify elements are ordered correctly.
     if list(input_metadata_fields) != REQUIRED_FIELDS:
-        raise ValidationError(f"Field(s) located incorrectly in {METADATA} for '{name}'. Expected order is {REQUIRED_FIELDS}.")
+        raise ValidationError(
+            f"Field(s) located incorrectly in {METADATA} for '{name}'. Expected order is {REQUIRED_FIELDS}."
+        )
 
     # Validate field values.
     for field in metadata:
@@ -208,30 +232,47 @@ def validate_required_fields(metadata: dict):
         if field == "tier":
             tier_val = metadata.get("tier")
             if tier_val not in TIER_OPTIONS:
-                raise ValidationError(f"Invalid 'tier' value in {METADATA} for '{name}': '{tier_val}'. Expected a scalar string from the following options: {TIER_OPTIONS}.")
+                raise ValidationError(
+                    f"Invalid 'tier' value in {METADATA} for '{name}': '{tier_val}'. "
+                    f"Expected a scalar string from the following options: {TIER_OPTIONS}."
+                )
 
         elif field == "stability":
             stability_val = metadata.get("stability")
             if stability_val not in STABILITY_OPTIONS:
-                raise ValidationError(f"Invalid 'stability' value in {METADATA} for '{name}': '{stability_val}'. Expected one of: {STABILITY_OPTIONS}.")
+                raise ValidationError(
+                    f"Invalid 'stability' value in {METADATA} for '{name}': '{stability_val}'. "
+                    f"Expected one of: {STABILITY_OPTIONS}."
+                )
 
         elif field == "dependencies":
             # Dependencies should be a dictionary.
             dependency_val = metadata.get("dependencies")
             if not isinstance(dependency_val, dict):
-                raise ValidationError(f"{value_type} value identified for field 'dependencies' in {METADATA} for '{name}'. Value must be array.")
+                raise ValidationError(
+                    f"{value_type} value identified for field 'dependencies' in {METADATA} "
+                    f"for '{name}'. Value must be array."
+                )
             dependency_types = set(dependency_val.keys())
 
             # Dependencies should contain 'kubeflow' and can contain 'external_services'.
             if not (dependency_types == {"kubeflow"} or dependency_types == {"kubeflow", "external_services"}):
-                raise ValidationError(f"The following field(s) were found in dependencies: {list(dependency_val.keys())}. Expected {DEPENDENCIES_FIELDS}.")
+                raise ValidationError(
+                    f"The following field(s) were found in dependencies: {list(dependency_val.keys())}. "
+                    f"Expected {DEPENDENCIES_FIELDS}."
+                )
 
             # Kubeflow Pipelines is a required dependency.
             kf_dependencies = dependency_val.get("kubeflow")
             ext_dependencies = dependency_val.get("external_services")
-            if not isinstance(kf_dependencies, list) or (ext_dependencies is not None and not isinstance(ext_dependencies, list)):
-                raise ValidationError(f"Dependency sub-types for '{name}' should contain lists but instead are {type(kf_dependencies)} and {type(ext_dependencies)}.")
-            kfp_present = any(d.get('name') == 'Pipelines' for d in kf_dependencies)
+            if not isinstance(kf_dependencies, list) or (
+                ext_dependencies is not None and not isinstance(ext_dependencies, list)
+            ):
+                raise ValidationError(
+                    f"Dependency sub-types for '{name}' should contain lists but instead are "
+                    f"{type(kf_dependencies)} and {type(ext_dependencies)}."
+                )
+            kfp_present = any(d.get("name") == "Pipelines" for d in kf_dependencies)
             if not kfp_present:
                 raise ValidationError(f"{METADATA} for '{name}' is missing Kubeflow Pipelines dependency.")
 
@@ -246,29 +287,50 @@ def validate_required_fields(metadata: dict):
             # Dependency versions must be correctly formatted by semantic versioning.
             invalid_dependencies = get_invalid_versions(kf_dependencies) + get_invalid_versions(ext_dependencies)
             if len(invalid_dependencies) > 0:
-                raise ValidationError(f"{METADATA} for '{name}' contains one or more dependencies with invalid semantic versioning: {invalid_dependencies}.")
+                raise ValidationError(
+                    f"{METADATA} for '{name}' contains one or more dependencies with invalid "
+                    f"semantic versioning: {invalid_dependencies}."
+                )
 
         elif field == "tags":
             tags_val = metadata.get("tags")
             if not (isinstance(tags_val, list)):
-                raise ValidationError(f"{value_type} value identified in field 'tags' in {METADATA} for '{name}'. Value must be string array.")
+                raise ValidationError(
+                    f"{value_type} value identified in field 'tags' in {METADATA} for '{name}'. "
+                    f"Value must be string array."
+                )
             if not all(isinstance(item, str) for item in tags_val):
-                raise ValidationError(f"The following tags in {METADATA} for '{name}': {tags_val}. Expected an array of scalar strings.")
+                raise ValidationError(
+                    f"The following tags in {METADATA} for '{name}': {tags_val}. Expected an array of scalar strings."
+                )
         elif field == "ci":
             ci_val = metadata.get("ci")
             if not isinstance(ci_val, dict):
-                raise ValidationError(f"{value_type} value identified for field 'ci' in {METADATA} for '{name}'. Value must be dictionary.")
+                raise ValidationError(
+                    f"{value_type} value identified for field 'ci' in {METADATA} for '{name}'. "
+                    f"Value must be dictionary."
+                )
             keys = set(ci_val.keys())
             if not (keys == {"skip_dependency_probe"}):
-                raise ValidationError(f"The following field(s) were found in field 'ci' in {METADATA} for '{name}': {list(ci_val.keys())}. Only field 'skip_dependency_probe' is valid.")
+                raise ValidationError(
+                    f"The following field(s) were found in field 'ci' in {METADATA} for '{name}': "
+                    f"{list(ci_val.keys())}. Only field 'skip_dependency_probe' is valid."
+                )
             probe = ci_val.get("skip_dependency_probe")
             if probe is not None and not isinstance(probe, bool):
-                raise ValidationError(f"{METADATA} expects a boolean value for skip_dependency_probe but {type(probe).__name__} value provided: '{probe}'.")
+                raise ValidationError(
+                    f"{METADATA} expects a boolean value for skip_dependency_probe but "
+                    f"{type(probe).__name__} value provided: '{probe}'."
+                )
 
         elif field == "links":
             links_value = metadata.get("links")
             if not isinstance(links_value, dict):
-                raise ValidationError(f"{value_type} value identified in field 'links' in {METADATA} for '{name}'. Value must be dictionary.")
+                raise ValidationError(
+                    f"{value_type} value identified in field 'links' in {METADATA} for '{name}'. "
+                    f"Value must be dictionary."
+                )
+
 
 def get_invalid_versions(dependencies: list[dict]) -> list[dict]:
     """Return a list of the input dependencies that contain invalid semantic versioning.
@@ -281,7 +343,7 @@ def get_invalid_versions(dependencies: list[dict]) -> list[dict]:
     """
     if dependencies is None:
         return []
-    invalid : list[dict] = []
+    invalid: list[dict] = []
     for dependency in dependencies:
         version = dependency.get("version")
         # If the dependency version is null or non-string, it is invalid.
@@ -294,6 +356,7 @@ def get_invalid_versions(dependencies: list[dict]) -> list[dict]:
         if not Version.is_valid(version):
             invalid.append(dependency)
     return invalid
+
 
 def main():
     """Main entry point for the CLI."""
@@ -319,6 +382,7 @@ def main():
     # Validation successful.
     logging.info(f"Validation successful for {input_dir}.")
     print(f"Validation successful for {input_dir}.")
+
 
 if __name__ == "__main__":
     main()
