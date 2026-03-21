f2c198fd2d5a61b696fb75dacfc9f488dbfff0ba
Yi Zheng <237498169+beatsmonster@users.noreply.github.com>
2026-03-21 14:58:03 -0400

    fix: rename test_data to tests/data and use kfp.kubernetes import path
    Signed-off-by: Yi Zheng <237498169+beatsmonster@users.noreply.github.com>

--- pipelines/data_processing/sdg/pipeline.py
+++ pipelines/data_processing/sdg/pipeline.py
@@ -5,7 +5,7 @@ and produces generated output as a KFP artifact.
 """
 
 from kfp import compiler, dsl
-from kfp_kubernetes import use_config_map_as_volume, use_secret_as_env
+from kfp.kubernetes import use_config_map_as_volume, use_secret_as_env
 
 from components.data_processing.sdg.component import sdg
 
